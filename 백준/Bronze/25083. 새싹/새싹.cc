64bead8002786777d7bc26cb118ea46d79a850ad
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:19:27 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+cout << "         ,r\'\"7" << '\n';
+cout << "r`-_   ,\'  ,/" << '\n';
+cout << " \\. \". L_r\'" << '\n';
+cout << "   `~\\/" << '\n';
+cout << "      |" << '\n';
+cout << "      |" << '\n';
+}
+
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    solve();
+    
+    return 0;
+}
