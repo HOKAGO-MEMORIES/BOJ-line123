86247244ff3b67b7a0edf77f1513317fec0bbdbc
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:11:44 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+
+	string s;
+	cin >> s;
+
+	cout << s.length() << '\n';
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+	solve();
+
+    return 0;
+}
