fdd8b48eb8236b638951400aa3e0984d0e55a851
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:07:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+
+	cout << "1" << '\n' << "0" << '\n';
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+	solve();
+
+    return 0;
+}
