a9e20904a3d228e17e671c60fec5aa029f7ffe96
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:33:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1888 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int num[10001] = { 0 };
+	int a;
+
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		num[a]++;
+	}
+
+	for (int j = 1; j < 10001; j++) {
+		if (num[j]) {
+			for (int k = 0; k < num[j]; k++)
+				cout << j << '\n';
+		}
+	}
+	
+	return 0;
+}
