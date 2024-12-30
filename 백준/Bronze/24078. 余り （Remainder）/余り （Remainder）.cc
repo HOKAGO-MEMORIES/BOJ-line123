c558975cafb705c3e060fc452ad735ce0aa55732
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:20:37 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x;
+    cin >> x;
+    cout << x % 21 << '\n';
+
+	return 0;
+}
