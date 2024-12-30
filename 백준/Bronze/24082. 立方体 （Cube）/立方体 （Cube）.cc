abc3aedc142a95b78c9cf6917352c783c113e057
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:21:03 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int x;
+    cin >> x;
+    cout << x * x * x << '\n';
+    
+    return 0;
+}
