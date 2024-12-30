e87345a25201dcdc7b220c53c058b049c9ff2547
Kim YongHwi <minchotejava@gmail.com>
2024-12-30 23:22:04 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	cout << b - a << " " << b << '\n';
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+
+	return 0;
+}
