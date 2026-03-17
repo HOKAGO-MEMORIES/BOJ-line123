5a370d7cb7ca3a9dcee19e8b4118d05ee2fd0db6
Kim YongHwi <minchotejava@gmail.com>
2026-03-17 23:26:48 +0900

    [Gold IV] Title: KSA 문자열, Time: 164 ms, Memory: 20440 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.java
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.java
@@ -10,10 +10,10 @@ public class Main {
     }
 
     static int solve(String X) {
-        int kas = findString(X, "KSA");
+        int ksa = findString(X, "KSA");
         int sak = Math.min(findString(X, "SAK"), X.length() - 1);
         int aks = Math.min(findString(X, "AKS"), X.length() - 2);
-        int ans = Math.max(Math.max(kas, sak), aks);
+        int ans = Math.max(Math.max(ksa, sak), aks);
         return (X.length() - ans) * 2;
     }
 
