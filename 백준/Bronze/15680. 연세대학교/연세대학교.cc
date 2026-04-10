38119f70a1fcfd65c41cb7952bfaf42bf8ff59e0
Kim YongHwi <minchotejava@gmail.com>
2026-04-10 23:25:58 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    if (n == 0) cout << "YONSEI" << '\n';
+    else cout << "Leading the Way to the Future" << '\n';
+
+    return 0;
+}
