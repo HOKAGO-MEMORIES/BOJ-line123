c5476d4bb70967dc8c4c47452e86ca70f0ddf8df
Kim YongHwi <minchotejava@gmail.com>
2026-04-17 16:43:20 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -2,54 +2,55 @@
 #include <queue>
 using namespace std;
 
-int board[101];
-int ans[101];
+int map[101];
+int dist[101];
+queue<int> q;
 
-void solve() {
-	int n, m, s, e;
-	cin >> n >> m;
+void bfs() {
+    while(!q.empty()) {
+        int now = q.front();
+        q.pop();
 
-	queue<int> q;
+        for (int i = 1; i <= 6; i++) {
+            int next = now + i;
 
-	for (int i = 0; i < m + n; i++) {
-		cin >> s >> e;
-		board[s] = e;
-	}
+            if (next > 100)
+                continue;
 
-	q.push(1);
-
-	while (!q.empty()) {
-		int now = q.front();
-		q.pop();
-
-		for (int i = 1; i <= 6; i++) {
-			int next = now + i;
-
-			if (next > 100)
-				continue;
-
-			if (board[next] != 0) {
-				next = board[next];
-			}
-			
-			if (!ans[next]) {
-				ans[next] = ans[now] + 1;
-				q.push(next);
-			}
-			
-		}
-	}
-
-	cout << ans[100] << '\n';
+            if (map[next])
+                next = map[next];
+            
+            if (!dist[next]) {
+                dist[next] = dist[now] + 1;
+                q.push(next);
+            }
+        }
+    }
 }
 
+void solve() {
+    int n, m, start, end, total;
+    cin >> n >> m;
+    total = n + m;
+
+    while(total--) {
+        cin >> start >> end;
+        map[start] = end;
+    }
+
+    q.push(1);
+    bfs();
+
+    cout << dist[100] << '\n';
+}
+
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    solve();
 
-	solve();
-
-	return 0;
+    return 0;
 }
