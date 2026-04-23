9cb709d7d95d2a2ffdcdc0243f36f89c68a96c8c
Kim YongHwi <minchotejava@gmail.com>
2026-04-23 22:54:35 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+
+void solve() {
+    for (int i = 0; i < n; i++)
+        cout << "LoveisKoreaUniversity" << " ";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    solve();
+    return 0;
+}
